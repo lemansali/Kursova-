89dd0ad66ae5a9a70584aa82d63014200143ba70
lemansali <79211103+lemansali@users.noreply.github.com>
2021-04-20 00:44:30 +0300

    Add files via upload

--- www/js/index.js
+++ www/js/index.js
@@ -2,69 +2,15 @@ document.addEventListener('deviceready', onDeviceReady, false);
 
 function onDeviceReady() {
     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-    window.addEventListener("batterystatus", onBatteryStatus, false);
-
     applyDeviceData();
     checkConnection();
-  
+
+    navigator.geolocation.watchPosition(geolocationSuccess,geolocationError);
+    window.addEventListener("batterystatus", onBatteryStatus, false);
     
-    navigator.geolocation.watchPosition(geolocationSuccess, geolocationError);
-    
-    document.addEventListener("offline", onOffline, false);
-    document.addEventListener("online", onOnline, false);
-    $("#mediabtn").click(audioFunction);    
-    $("#camera-btn").click(getPicture);
+    $("#camera-btn").click(getPicutre);
     $("#addPicture").click(sendPictureRequest);
 }
-function getPicutre() {
-    navigator.camera.getPicture(succeededCameraCallback, failedCameraCallback, {
-        quality: 25,
-        destinationType: Camera.DestinationType.DATA_URL
-    });
-}
-function succeededCameraCallback(imageData) {
-    $('#myImage').attr('src', 'data:image/jpeg;base64,' + imageData);
-}
-
-function failedCameraCallback(message) {
-    alert(message);
-}
-function sendPictureRequest() {
-    $.mobile.loading("show");
-
-    let base64Img = $('#myImage').attr('src');
-    let facultyNumber = $('#faculty-number').val();
-    let password = $('#password').val();
-
-    let requestData = {
-        FacultyNumber: facultyNumber,
-        Password: password,
-        Picture: base64Img
-    };
-    console.log(requestData);
-
-    $.ajax({
-        type: "PUT",
-        url: baseSurviceUrl + "picture",
-        contentType: "application/json",
-        dataType: "json",
-        data: JSON.stringify(requestData),
-        success: function() {
-            $.mobile.loading("hide");
-            alert("Successfully added!");
-            loadDetailsPage();
-        },
-        error: function(XMLHttpRequest, textStatus, errorThrown) {
-            $.mobile.loading("hide");
-            loadDetailsPage();
-        }
-    });
-}
-function loadHomePage(){
-    const homePage = $("#homePage");
-    $.mobile.pageContainer.pageContainer("change", homePage,{});
-}
-
 
 function applyDeviceData() {
     $("#cordovaVersion").text(device.cordova);
@@ -85,7 +31,7 @@ function onBatteryStatus(status) {
 
 function checkConnection() {
     var networkState = navigator.connection.type;
-
+    
     var states = {};
     states[Connection.UNKNOWN] = 'Unknown connection';
     states[Connection.ETHERNET] = 'Ethernet connection';
@@ -95,55 +41,80 @@ function checkConnection() {
     states[Connection.CELL_4G] = 'Cell 4G connection';
     states[Connection.CELL] = 'Cell generic connection';
     states[Connection.NONE] = 'No network connection';
-
+    
     $("#connectionType").text(states[networkState]);
 }
 
 function geolocationSuccess(position) {
-    $("#latitude-value").text(position.coords.latitude);
-    $("#longitude-value").text(position.coords.longitude);
-    $("#altitude-value").text(position.coords.altitude);
-    $("#accuracy-value").text(position.coords.accuracy);
-    $("#altitudeAccuracy-value").text(position.coords.altitudeAccuracy);
+    $('#latitude-value').text(position.coords.latitude);
+    $('#longitude-value').text(position.coords.longitude);
+    $('#altitude-value').text(position.coords.altitude);
+    $('#accuracy-value').text(position.coords.accuracy);
+    $('#altitudeAccuracy-value').text(position.coords.altitudeAccuracy);
 }
 
 function geolocationError(error) {
-    alert(error.message);
+    alert(error);    
 }
 
-function onOffline() {
-    $("#status-value").text("The app is running in offline mode!");
+//Picture
+
+function getPicutre() {
+    navigator.camera.getPicture(succeededCameraCallback, failedCameraCallback, {
+        quality: 25,
+        destinationType: Camera.DestinationType.DATA_URL
+    });
 }
 
-function onOnline() {
-    $("#status-value").hide();
+function succeededCameraCallback(imageData) {
+    $('#myImage').attr('src', 'data:image/jpeg;base64,' + imageData);
+    $('#myImage').css('display', 'block');
+    $('#myImage').show();
 }
 
-function audioFunction() {
-    var my_media = new Media('cdvfile://localhost/temporary/recording.mp3', onSuccess, onError);
-    my_media.startRecord();
-    var mediaTimer = setInterval(function() {
-        // get media amplitude
-        my_media.getCurrentAmplitude(
-            // success callback
-            function(amp) {
-                console.log(amp + "%");
-            },
-            // error callback
-            function(e) {
-                console.log("Error getting amp=" + e);
-            }
-        );
-    }, 1000);
+function failedCameraCallback(message) {
+    alert(message);
 }
 
-function onSuccess(params) {
-    alert(params);
+function sendPictureRequest() {
+    $.mobile.loading("show");
+
+    let base64Img = $("#myImage").attr('src');
+    let userName = $('#username').val();
+    let password = $('#password').val();
+
+    let requestData = {
+        UserName: userName,
+        Password: password,
+        Picture: base64Img
+    };
+
+    console.log({ requestData });
+
+    $.ajax({
+        type: "PUT",
+        url: baseSurviceUrl + "picture",
+        contentType: "application/json",
+        dataType: "json",
+        data: JSON.stringify(requestData),
+        success: function() {
+            $.mobile.loading("hide");
+            alert("Successfully added!");
+            loadDetailsPage();
+        },
+        error: function(XMLHttpRequest, textStatus, errorThrown) {
+            $.mobile.loading("hide");
+            loadDetailsPage();
+        }
+    });
 }
 
-function onError(params) {
-    alert(params);
+function loadHomePage() {
+    const homePage = $("#page1");
+    $.mobile.pageContainer.pagecontainer("change", homePage, {});
 }
+
+//kinvey 
 function startApp() {
     const kinveyBaseUrl = "https://baas.kinvey.com/";
     const kinveyAppKey = "kid_rJ87qhF8d";
